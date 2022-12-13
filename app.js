c27a6868064430bfbc4c26d7f6fe76be9886fa81
RouterRico <ricozethof@gmail.com>
2022-12-13 12:41:24 +0100

    .

--- app.js
+++ app.js
@@ -1,101 +1,59 @@
-﻿// Importing express module
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001;
 
-let data;
+app.get("/", (req, res) => res.type('html').send(html));
 
-let allGames = [];
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/game', function(req, res) {
-    res.sendFile(__dirname + '/game.html');
-});
-
-app.get('/getGameData', (req, res) => {
-    console.log('got get');
-    allGames = [];
-    const jsonsInDir = fs.readdirSync('./JsonFiles').filter(file => path.extname(file) === '.json');
-
-    jsonsInDir.forEach(file => {
-        const fileData = fs.readFileSync(path.join('./JsonFiles', file));
-        const json = JSON.parse(fileData.toString());
-        console.log(json['Date']);
-
-        if(json['Date'] === req.query.Game)
-        {
-            console.log(json);
-            allGames.push(json);
-        }
-    });
-
-    var tryFetch = {AllGames: allGames};
-
-    res.json(tryFetch)
-});
-
-app.post('/postData', (req, res) => {
-    console.log('got post');
-
-    data = JSON.stringify(req.body);
-    console.log(data);
-    fs.writeFile('JsonFiles/' + req.body.FileName + '.json', data, 'utf8', callback => console.log('done'));
-    
-    res.send(req.body);
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
-
-app.get('/getData', (req, res) => {
-    console.log('got get');
-    allGames = [];
-    const jsonsInDir = fs.readdirSync('./JsonFiles').filter(file => path.extname(file) === '.json');
-
-    jsonsInDir.forEach(file => {
-        const fileData = fs.readFileSync(path.join('./JsonFiles', file));
-        let json = JSON.parse(fileData.toString());
-
-        allGames.push(json);
-    });
-
-    allGames.sort(function(a, b) {
-        return new Date(b['Date']) - new Date(a['Date']);
-    });
-
-    console.log(allGames);
-
-    var tryFetch = {AllGames: allGames};
-
-    res.json(tryFetch)
-});
-
-app.listen(3000, () => {
-    console.log('Our express server is up on port 3000');
-});
-
-function dynamicSort(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        /* next line works with strings and numbers,
-         * and you may want to customize it to your needs
-         */
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
-    }
-}
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
